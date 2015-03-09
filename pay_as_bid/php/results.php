0473b022b0cd9883bf11c42d01604f628faa7c90
TueVJ <tue.vissing.jensen@gmail.com>
2015-03-09 12:34:56 +0100

    results to sum

--- pay_as_bid/php/results.php
+++ pay_as_bid/php/results.php
@@ -27,6 +27,7 @@ if (isset($_COOKIE["userID"])) {
 }
 
 $guessarray = array();
+$profitsumarray = array();
 $file = fopen("php/stats.txt", "r") or exit("Unable to open file!");
 //Output a line of the file until the end is reached
 while(!feof($file))
@@ -34,13 +35,20 @@ while(!feof($file))
 	$line=fgets($file);
 	if(!empty($line)){
 		$part=explode(",",$line);
+    if(!(in_array($part[0], profitsumarray))){
+      $profitsumarray[$part[0]] = 0.00
+    }
 		$guessarray[$part[0]] = array($part[1], $part[2], $part[3], $part[4]);
+    $profitsumarray[$part[0]] += number_format($guessarray[$userID][3],2)
 	}
   }
 fclose($file);
 
 if(array_key_exists($userID,$guessarray)){
-    echo "<p><font size = \"4\" >ID: " . number_format($userID,0) . " last produced " . number_format($guessarray[$userID][1],0) . " MWh of which " . number_format($guessarray[$userID][2],0) . " was scheduled at " . number_format($guessarray[$userID][0],2) ."&#36;&#47;MW. </p> <p>A total of " . number_format($guessarray[$userID][3],2) . "&#36; earned so far.</font></p>";
+    echo "<p><font size = \"4\" >ID: " . number_format($userID,0) .
+    " last produced " . number_format($guessarray[$userID][1],0) . " MWh of which " .
+    number_format($guessarray[$userID][2],0) . " was scheduled at " . number_format($guessarray[$userID][0],2) ."&#36;&#47;MW. </p>
+    <p>A total of " . number_format($profitsumarray[$userID][3],2) . "&#36; earned so far.</font></p>";
 } else {
 
     echo "<p><font size = \"4\" >ID: " . number_format($userID,2) . " not found.</font></p>";
