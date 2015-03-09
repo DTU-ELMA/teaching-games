49db7676fbcc0de6aaeb73b040ecbc3b4d5d8555
TueVJ <tue.vissing.jensen@gmail.com>
2015-03-09 05:43:27 -0400

    home files change

--- guess_average/php/home.php
+++ guess_average/php/home.php
@@ -3,6 +3,6 @@
 <p> The rules of the game are simple:
 Each participant bids a number between 0 and 100.
 You can change your bid by bidding again.
-The winner(s) will be the person(s) who is closest to 2/3rds of the average of all bids.</p>
+The winner will be the person who is closest to 2/3rds of the average of all bids.</p>
 
-<p> Before proceeding to place a bid, please take a moment to think about how to bid in order to maximise your chance of winning.</p>
+<!-- <p> Before proceeding to place a bid, please take a moment to think about how to bid in order to maximise your chance of winning.</p> -->
